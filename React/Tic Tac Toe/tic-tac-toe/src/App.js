c68ce71a55195dd0f6de5e6b1687921368d54d99
zoe <zoeeustace@hotmail.com>
2023-09-13 13:38:55 +0100

    completed noughts and crosses game

--- React/Tic Tac Toe/tic-tac-toe/src/App.js
+++ React/Tic Tac Toe/tic-tac-toe/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cell from "./components/cell";
 
 const App = () => {
@@ -9,7 +9,35 @@ const App = () => {
 
   const message = "it is now " + go + "'s go"
 
-  // console.log(cells);
+  console.log(cells);
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
+
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === "circle")
+      if (circleWins){
+        setWinningMessage("Cicle wins!")
+        return
+      }
+    })
+
+    winningCombos.forEach(array => {
+      let crossWins = array.every(cell => cells[cell] === "cross")
+      if (crossWins){
+        setWinningMessage("Cross wins!")
+        return
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkScore()
+  }, [cells])
 
   return (
     <div className="app">
@@ -18,13 +46,15 @@ const App = () => {
       <Cell 
       key={index} 
       id={index} 
-      cell={cell} 
+      cell={cell}
+      cells={cells} 
       setCells={setCells}
       go={go}
-      setGo={setGo}/>)}
+      setGo={setGo}
+      winningMessage={winningMessage}/>)}
       </div>
       <p>
-        {message}
+        {winningMessage || message}
       </p>
     </div>
   );
--- React/Tic Tac Toe/tic-tac-toe/src/components/cell.js
+++ React/Tic Tac Toe/tic-tac-toe/src/components/cell.js
@@ -1,23 +1,38 @@
-const Cell = ({id, cell, setCells, go, setGo}) => {
+const Cell = ({id, cell, setCells, go, setGo, cells, winningMessage}) => {
 
     const handleClick = (event) => {
+        if (event.target.className === 'circle') return
+        if (event.target.className === 'cross') return
         const taken = event.target.firstChild.classList.contains("circle") || 
         event.target.firstChild.classList.contains("cross")
 
         if (!taken){
             if (go === "circle"){
                 event.target.firstChild.classList.add("circle")
+                handleCellChange("circle")
                 setGo("cross")
             }
             if (go === "cross"){
                 event.target.firstChild.classList.add("cross")
+                handleCellChange("cross")
                 setGo("circle")
             }
         }
     }
 
+    const handleCellChange = (className) => {
+        const nextCells = cells.map((cell, index) => {
+            if (index === id) {
+                return className
+            } else {
+                return cell
+            }
+        })
+        setCells(nextCells)
+    }
+
     return ( 
-        <div className="square" id={id} onClick={handleClick}>
+        <div className="square" id={id} onClick={!winningMessage ? handleClick: undefined}>
             <div className={cell}></div>
         </div>
      );
